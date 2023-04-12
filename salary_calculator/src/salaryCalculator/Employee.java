3770d5973998f83350b97c43c0df855b2dbd4b3d
isaiasmtp <isaias.pontes@ccc.ufcg.edu.br>
2023-04-12 09:47:32 -0300

    create project

---
+++ salary_calculator/src/salaryCalculator/Employee.java
@@ -0,0 +1,5 @@
+package salaryCalculator;
+
+public class Employee {
+
+}
---
+++ salary_calculator/src/salaryCalculator/EmployeeTest.java
@@ -0,0 +1,14 @@
+package salaryCalculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmployeeTest {
+
+	@Test
+	public void test() {
+		assert 1 == 1;
+	}
+
+}
